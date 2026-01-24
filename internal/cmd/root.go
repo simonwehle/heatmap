4d9bb5f254be5e717b4b98a17c676c1ae7c598b2
simonwehle <simon.wehle@gmx.net>
2026-01-24 11:49:43 +0100

    add maplibre

--- internal/cmd/root.go
+++ internal/cmd/root.go
@@ -6,10 +6,8 @@ import (
 )
 
 func Execute() {
-	fileServer := http.FileServer(http.Dir("."))
-	http.Handle("/", fileServer)
-
-	addr := ":8000"
-	log.Println("Serving on http://localhost" + addr)
+	http.Handle("/", http.FileServer(http.Dir("./web")))
+	addr := ":8080"
+	log.Println("Server started at port" + addr)
 	log.Fatal(http.ListenAndServe(addr, nil))
 }
